799202a4476bb6f7df1e80b6a4c97d50100eafa1
Plamen Kinev <plamen@node01.prosrm.com>
2018-08-09 16:04:22 +0000

    Added changes to bash file

--- check_logs.sh
+++ check_logs.sh
@@ -1,24 +1,49 @@
 #!/usr/bin/env bash
 
-CONF_FILE="check_logs.conf"
+set -x
+
+APP_LOGS_DIR="/home/plamen/dir1/dir2/dir3/dir4"
+THRESHOLD=75
+NOTIFY_EMAILS="plamenkinev@gmail.com"
+LOG_FILE="check_logs.log"
 
 get_disk_utilization() {
 	DIR=$1
 	DISK_UTIL=$(df -h $DIR | tail -1 | awk '{print $5}' | cut -d '%' -f 1)
+	DISK_UTIL=80
 }
 
-if [ -f $CONF_FILE ]; then 
-	. check_logs.conf
-	echo "$($LOG_DATE_FMT) Using config file $CONF_FILE ..." >> $LOG_FILE
-else
-	THRESHOLD_WARN=60
-	THRESHOLD_CRIT=75
-	APP_LOGS_DIR="/var/log/app/"
-	NOTIFY_EMAILS="admin@company.com"
-	LOG_FILE=$(dirname $0)/check_logs.log
-	LOG_DATE_FMT="date \"+%Y-%m-%d %H:%M:%s\""
-	echo "$($LOG_DATE_FMT) Using default config values ..." >> $LOG_FILE
-fi
+compress_uncompressed_logs() {
+	DIR=$1
+	cd $DIR
+	# Compress all logs without the one from today
+	find . -maxdepth 1 -type f -mtime +0 -exec xz -9 {} \;
+}
 
-get_disk_utilization $APP_LOGS_DIR
-echo $DISK_UTIL
+remove_log() {
+	DIR=$1
+	cd $DIR
+	COMPRESSED_LOGS=$(find . -maxdepth 1 -type f -name "*.xz")
+	COMPRESSED_LOGS_COUNT=$(echo $LOGS | wc -w)
+	OLD_COMPRESSED_LOG=$(find . -maxdepth 1 -type f -mtime +0 -printf '%Ts\t%p\n' | sort -n | head -1 | awk '{print $2}')
+	if [ $COMPRESSED_LOGS_COUNT -gt 0 ]; then
+		rm $OLD_COMPRESSED_LOG
+		$COMPRESSED_LOGS_COUNT=$(($COMPRESSED_LOGS_COUNT - 1))
+	fi
+}
+
+free_space() {
+	DIR=$1
+	get_disk_utilization $APP_LOGS_DIR
+	while [ $DISK_UTIL -ge $THRESHOLD ]; do
+		compress_uncompressed_logs $DIR
+		get_disk_utilization $DIR
+		if [ $DISK_UTIL -ge $THRESHOLD ] && [ $COMPRESSED_LOGS_COUNT -gt 0 ]; then
+			remove_log $DIR
+		else
+			mail -s "[CRITICAL] App logs file system need you intervention!" 
+		fi
+	done	
+}
+
+free_space
